033f48a26c4998a1544df52665963fcca9a275d6
aakanksha11 <aakankshasareen.gh@gmail.com>
2021-07-20 15:30:35 +0530

    add obj

--- bugFile.js
+++ bugFile.js
@@ -1,32 +1,39 @@
 import Axios from "axios";
 
-
+const defaultConfig = {
+    key: "QHixYrGS9EHSZqLJSXCwduKj1lC9Dv7NIl95GN9vPmXVm12vzj9KKUpHzQzIYA6j",
+    user : "Aakanksha",
+    logger: "Admin",
+    source: "console",
+    severity: 5,
+    properties : "-"
+}
 export const bugFileFunction = {
-    typeErr(error, vm, info, ){
-        this.common(error.toString(), vm, info)
+    typeErr(config,error, vm, info, ){
+        this.common(config,error.toString(), vm, info)
     },
-    warning(msg,vm,info){
-        this.common(msg, vm, info)
+    warning(config,msg,vm,info){
+        this.common(config,msg, vm, info)
     },
-    network(error, reason){
-        this.common( JSON.stringify(error), '', reason.toString())
+    network(config,error, reason){
+        this.common( config,JSON.stringify(error), '', reason.toString())
     },
-    common(error,url,info){
+    common(config, error,url,info){
         fetch('https://logs.kartmax.in/api/logs', {
         method: 'post',
         headers: {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json',
-            'X-Authorization': 'QHixYrGS9EHSZqLJSXCwduKj1lC9Dv7NIl95GN9vPmXVm12vzj9KKUpHzQzIYA6j'
+            'X-Authorization':  (config.key == "") ? defaultConfig.key: config.key,
         },
         body: JSON.stringify({
             "log":  error,
-            "user": "Aakanksha",
-            "logger": "Admin",
-            "source": "console",
+            "user": (config.user == "") ? defaultConfig.user: config.user,
+            "logger":  (config.logger == "") ? defaultConfig.logger: config.logger,
+            "source": (config.losourcegger == "") ? defaultConfig.source: config.source,
             "message": info,
-            "severity": 5,
-            "properties": "-"
+            "severity":(config.severity == "") ? defaultConfig.severity: config.severity,
+            "properties": (config.properties == "") ? defaultConfig.properties: config.properties,
         })
         }).then(res => res.json())
             .then(res => console.log(res));
